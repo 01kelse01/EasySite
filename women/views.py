4c1ffd851a0131ba81326a93bafdf941d9c1f818
kelse <skyrit999@gmail.com>
2023-03-14 14:56:24 +0200

    add urls.py in women and add categories

--- women/views.py
+++ women/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Сторінка програми women.")
+
+
+def categories(request):
+    return HttpResponse("<h1> Відображення категорій </h1>")
