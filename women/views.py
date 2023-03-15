811cb041a2bf919adae5931d303b8e3c05af3cf1
kelse <skyrit999@gmail.com>
2023-03-15 23:17:05 +0200

    changed show_category in women/views

--- women/views.py
+++ women/views.py
@@ -48,7 +48,16 @@ def show_post(request, post_id):
 
 
 def show_category(request, cat_id):
-    return HttpResponse(f'Відображення по  id = {cat_id}')
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Відображення по рубрикам',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def pageNotFound(request, exception):
