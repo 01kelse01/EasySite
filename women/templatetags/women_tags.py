60021e9347ac45868c4d8c62e83aaeb5a92cc514
kelse <skyrit999@gmail.com>
2023-03-16 16:39:28 +0200

    changed get_categories and add show_categories

--- women/templatetags/women_tags.py
+++ women/templatetags/women_tags.py
@@ -4,6 +4,12 @@ from women.models import *
 register = template.Library()
 
 
-@register.simple_tag(name='getcats')
+@register.simple_tag(name='getcats')  # Простий тег
 def get_categories():
     return Category.objects.all()
+
+
+@register.inclusion_tag('women/list_categories.html')  # Включающий тег
+def show_categories():
+    cats = Category.objects.all()
+    return {'cats': cats}
