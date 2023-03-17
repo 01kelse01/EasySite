aa1ecd80506e5955632c779c7ec5b53bc74ccbfe
kelse <skyrit999@gmail.com>
2023-03-17 09:48:32 +0200

    delete db and migrate new db

--- women/models.py
+++ women/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Категорія')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.name
@@ -19,12 +20,13 @@ class Category(models.Model):
 
 class Women(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True, verbose_name='Текст публікації')
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата створення')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Дата змінення')
     is_published = models.BooleanField(default=True, verbose_name='Опубліковано')
-    cat = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категорія')
+    cat = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категорія')
 
     def __str__(self):
         return self.title
