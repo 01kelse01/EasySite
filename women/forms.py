70c86c43e6ef5b9961b3ed8f6cc039fefbf135c3
kelse <skyrit999@gmail.com>
2023-03-17 15:34:15 +0200

    changed AddPostForm and addpage.html

--- women/forms.py
+++ women/forms.py
@@ -3,7 +3,7 @@ from .models import *
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок')
+    title = forms.CharField(max_length=255, label='Заголовок',widget=forms.TextInput(attrs={'class': 'form-input'}))
     slug = forms.SlugField(max_length=255, label='URL')
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Контент')
     is_published = forms.BooleanField(required=False, label='Публікація', initial=True)
