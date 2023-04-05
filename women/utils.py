ef2d2d429cbf3fcf3616a2f0f4264791b7859f18
kelse <skyrit999@gmail.com>
2023-04-05 16:48:04 +0300

    add debug_toolbar

--- women/utils.py
+++ women/utils.py
@@ -10,7 +10,8 @@ menu = [
 
 
 class DataMixin:
-    paginate_by = 3
+    paginate_by = 20
+
     def get_user_context(self, **kwargs):
         context = kwargs
         cats = Category.objects.annotate(Count('women'))
