e6b775da9ba45b74f4bee24d6a85c8492746e57d
kelse <skyrit999@gmail.com>
2023-04-05 20:17:51 +0300

    add captcha

--- women/views.py
+++ women/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
@@ -74,8 +74,25 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
 #     return render(request, 'women/addpage.html', context=context)
 
 
-def contact(request):
-    return HttpResponse('Зворотній зв`язок')
+# def contact(request):
+#     return HttpResponse('Зворотній зв`язок')
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'women/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Зворотній зв\'язок')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
+
+
+
+
 
 
 # def login(request):
