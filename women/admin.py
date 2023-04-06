96a34cc89d55f5754fceecdbfdb18f823ce6b38f
kelse <skyrit999@gmail.com>
2023-04-06 10:11:37 +0300

    changed WomenAdmin

--- women/admin.py
+++ women/admin.py
@@ -11,13 +11,15 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {'slug': ('title',)}
-    fields = ('title', 'slug', 'cat', 'content', 'photo', 'is_published')
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    # save_on_top = True
 
     def get_html_photo(self, object):
         if object.photo:
             return mark_safe(f'<img src="{object.photo.url}" width=50>')
 
-    get_html_photo.short_description = 'Фото'
+    get_html_photo.short_description = 'Зображення'
 
 
 class CategoryAdmin(admin.ModelAdmin):
