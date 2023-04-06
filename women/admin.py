c64034533c313da57717bc07cc4c563c0439c8c8
kelse <skyrit999@gmail.com>
2023-04-06 10:03:20 +0300

    add get_html_photo and changed files

--- women/admin.py
+++ women/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'is_published')
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f'<img src="{object.photo.url}" width=50>')
+
+    get_html_photo.short_description = 'Фото'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -21,3 +29,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Women, WomenAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Адмін - панель сайту про жінок'
+admin.site.site_header = 'Адмін - панель сайту про жінок'
